4a37b8d5609f7b708823a081e67aa91aa23c9b4d
karoll755 <99765404+karoll755@users.noreply.github.com>
2022-08-12 19:45:04 -0500

    Add files via upload

---
+++ proyecto  11.js
@@ -0,0 +1,18 @@
+
+function preload(){
+preload("ship-4.png");
+preload("ship-3.png");
+preload("ship-2.png");
+preload("ship-1.png");
+preload("sea.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
